15204a9e1780d9292f214542d4f915a88110018b
Pavel Sapezhka <me@weastur.com>
2025-03-19 15:08:10 +0200

    cmd: fix usage

--- cmd/replacer/main.go
+++ cmd/replacer/main.go
@@ -12,7 +12,12 @@ var version = "v0.0.0-dev1"
 
 func init() {
 	flag.Usage = func() {
-		fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "Usage:\n")
+		fmt.Fprintf(
+			os.Stderr,
+			"\nFill the config, put `//go:generate replacer` at the top of your source file.\n"+
+				"Refer to `go help generate` for more information about go code generators\n\nDefaults:\n",
+		)
 		flag.PrintDefaults()
 	}
 }
@@ -22,8 +27,8 @@ func main() {
 	flag.String(
 		"config",
 		"",
-		"Path to the configuration file (empty by default).\n"+
-			"If not provided, the generator will look for a config file (\".replacer.yml\") "+
+		"Path to the configuration file.\n"+
+			"If not provided (default), the generator will look for a config file (\".replacer.yml\") "+
 			"in the current directory, then move up to each parent directory until it reaches the root (the directory "+
 			"containing go.mod).\nIf no config file is found, the generator will do nothing and exit with 0 code",
 	)
