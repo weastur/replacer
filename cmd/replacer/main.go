ef961eb2a311c1ec70ac630ea9376262235f92e7
Pavel Sapezhka <me@weastur.com>
2025-03-19 14:49:13 +0200

    cmd: add version flag

--- cmd/replacer/main.go
+++ cmd/replacer/main.go
@@ -1,14 +1,31 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 
 	"github.com/weastur/replacer/internal/generator"
 )
 
-var version = "v0.0.0-dev1" //nolint:unused
+var version = "v0.0.0-dev1"
+
+func init() {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+}
 
 func main() {
+	versionFlag := flag.Bool("version", false, "Print the version and exit")
+	flag.Parse()
+
+	if *versionFlag {
+		fmt.Println(version)
+		os.Exit(0)
+	}
+
 	fmt.Println("Running my generator...")
 	generator.Run()
 }
